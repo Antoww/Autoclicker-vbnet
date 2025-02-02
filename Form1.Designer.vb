aa5959c71a523954f7ec74c07eab0de773ab4735
Antow <27694299+Antoww@users.noreply.github.com>
2025-02-02 19:01:32 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        ButtonStartStop = New Button()
+        NumericUpDown1 = New NumericUpDown()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        LinkLabel1 = New LinkLabel()
+        LinkLabel2 = New LinkLabel()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' ButtonStartStop
+        ' 
+        ButtonStartStop.Location = New Point(12, 105)
+        ButtonStartStop.Name = "ButtonStartStop"
+        ButtonStartStop.Size = New Size(305, 48)
+        ButtonStartStop.TabIndex = 0
+        ButtonStartStop.Text = "Start/Stop"
+        ButtonStartStop.UseVisualStyleBackColor = True
+        ' 
+        ' NumericUpDown1
+        ' 
+        NumericUpDown1.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        NumericUpDown1.Location = New Point(170, 20)
+        NumericUpDown1.Name = "NumericUpDown1"
+        NumericUpDown1.Size = New Size(143, 33)
+        NumericUpDown1.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(6, 14)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(158, 50)
+        Label1.TabIndex = 2
+        Label1.Text = "Clics per second :" & vbCrLf & "(max 1000/s)" & vbCrLf
+        Label1.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.Red
+        Label2.Location = New Point(63, 72)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(214, 21)
+        Label2.TabIndex = 3
+        Label2.Text = "Press ""F6"" to start and stop"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 156)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(49, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Github :"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(211, 156)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(50, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "Credits :"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(267, 156)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(47, 15)
+        LinkLabel1.TabIndex = 6
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "monkik"
+        ' 
+        ' LinkLabel2
+        ' 
+        LinkLabel2.AutoSize = True
+        LinkLabel2.Location = New Point(63, 156)
+        LinkLabel2.Name = "LinkLabel2"
+        LinkLabel2.Size = New Size(42, 15)
+        LinkLabel2.TabIndex = 7
+        LinkLabel2.TabStop = True
+        LinkLabel2.Text = "Antow"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(329, 176)
+        Controls.Add(LinkLabel2)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(NumericUpDown1)
+        Controls.Add(ButtonStartStop)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "Autoclicker VBNET"
+        TopMost = True
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ButtonStartStop As Button
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,132 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Threading
+Imports System.Diagnostics
+
+Public Class Form1
+    Private WithEvents TimerClick As New System.Windows.Forms.Timer()
+    Private Clicking As Boolean = False
+    Private ClicksPerSecond As Integer = 10 ' Par défaut, 10 clics par seconde
+
+    ' Définition des constantes pour les événements souris
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Sub mouse_event(dwFlags As Integer, dx As Integer, dy As Integer, cButtons As Integer, dwExtraInfo As Integer)
+    End Sub
+
+    Private Const MOUSEEVENTF_LEFTDOWN As Integer = &H2
+    Private Const MOUSEEVENTF_LEFTUP As Integer = &H4
+
+    ' HOOK clavier global
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function SetWindowsHookEx(idHook As Integer, lpfn As LowLevelKeyboardProc, hMod As IntPtr, dwThreadId As UInteger) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function UnhookWindowsHookEx(hhk As IntPtr) As Boolean
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function CallNextHookEx(hhk As IntPtr, nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Shared Function GetModuleHandle(lpModuleName As String) As IntPtr
+    End Function
+
+    Private Delegate Function LowLevelKeyboardProc(nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+
+    Private Const WH_KEYBOARD_LL As Integer = 13
+    Private Const WM_KEYDOWN As Integer = &H100
+
+    Private Shared KeyboardHookID As IntPtr = IntPtr.Zero
+    Private Shared HookCallback As LowLevelKeyboardProc = AddressOf KeyboardProc
+
+    ' Fonction de callback pour le hook clavier
+    Private Shared Function KeyboardProc(nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+        If nCode >= 0 AndAlso wParam = CType(WM_KEYDOWN, IntPtr) Then
+            Dim vkCode As Integer = Marshal.ReadInt32(lParam)
+            If vkCode = Keys.F6 Then
+                ' Appelle la fonction pour démarrer/arrêter l'autoclicker
+                Form1.ToggleClicker()
+            End If
+        End If
+        Return CallNextHookEx(KeyboardHookID, nCode, wParam, lParam)
+    End Function
+
+    ' Active/désactive le hook clavier global
+    Private Sub SetHook()
+        KeyboardHookID = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, GetModuleHandle(Nothing), 0)
+    End Sub
+
+    Private Sub RemoveHook()
+        UnhookWindowsHookEx(KeyboardHookID)
+    End Sub
+
+    ' Fonction pour activer/désactiver l'autoclicker
+    Public Shared Sub ToggleClicker()
+        Form1.Clicking = Not Form1.Clicking
+
+        If Form1.Clicking Then
+            Dim interval As Integer = 1000 \ Form1.ClicksPerSecond
+            Form1.TimerClick.Interval = interval
+            Form1.TimerClick.Start()
+        Else
+            Form1.TimerClick.Stop()
+        End If
+    End Sub
+
+    ' Événement du timer pour faire les clics
+    Private Sub TimerClick_Tick(sender As Object, e As EventArgs) Handles TimerClick.Tick
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+    End Sub
+
+    ' Démarrer/arrêter avec le bouton
+    Private Sub ButtonStartStop_Click(sender As Object, e As EventArgs) Handles ButtonStartStop.Click
+        ToggleClicker()
+    End Sub
+
+    ' Mise à jour du nombre de clics par seconde
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        ClicksPerSecond = CInt(NumericUpDown1.Value)
+        If Clicking Then
+            TimerClick.Interval = 1000 \ ClicksPerSecond
+        End If
+    End Sub
+
+    ' Initialisation de l'interface
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetHook() ' Active le hook clavier global au démarrage
+        NumericUpDown1.Minimum = 1
+        NumericUpDown1.Maximum = 1000
+        NumericUpDown1.Value = 10
+    End Sub
+
+    ' Désactive le hook lors de la fermeture du programme
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        RemoveHook()
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim urlico As String = "https://www.flaticon.com/fr/icones-gratuites/clic-de-souris"
+
+        Try
+            Process.Start(New ProcessStartInfo(urlico) With {
+                .UseShellExecute = True
+            })
+        Catch ex As Exception
+            MessageBox.Show("Impossible d'ouvrir le lien demandé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Dim urlgit As String = "https://github.com/Antoww"
+
+        Try
+            Process.Start(New ProcessStartInfo(urlgit) With {
+                .UseShellExecute = True
+            })
+        Catch ex As Exception
+            MessageBox.Show("Impossible d'ouvrir le lien demandé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+End Class
